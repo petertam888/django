f5dd32677def32301e3395c6e2a279662a6fa181
Tsz Fung Tam <petertam0712@hotmail.com>
2022-11-24 00:11:53 +0800

    Updated on 0011, 24/11/2022

--- src/trydjango/urls.py
+++ src/trydjango/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from pages.views import home_view, about_view, contact_view, social_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_view, name='home'),
+    path('about/', about_view),
+    path('contact/', contact_view),
+    path('social/', social_view),
 ]
